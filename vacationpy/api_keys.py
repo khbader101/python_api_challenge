751c299f318db714bf15e82f5c03258d0a681aa5
khbader101 <69255197+khbader101@users.noreply.github.com>
2020-10-27 09:43:02 -0400

    test

--- vacationpy/api_keys.py
+++ vacationpy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "bc5c1eea15e1698b4d08fd6d5c2dd9f2"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyAqhYbyYYD8aupa0VEaD5sArBvmE6g9nQo"
